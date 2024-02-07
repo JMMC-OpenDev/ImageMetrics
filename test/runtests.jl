e0a72c1c70e7df6dea16ea32e54b2715f760b4cd
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2024-02-07 10:55:54 +0100

    Interpolation methods

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,25 @@ using ImageMetrics
 using Test
 
 @testset "ImageMetrics.jl" begin
-    # Write your tests here.
+    @testset "Interpolation" begin
+        x = -4.3:0.1:5.3
+        y = -1.3:0.1:8.3
+        i = firstindex(x):0.3:lastindex(x)
+        j = firstindex(y)-2.1:0.3:lastindex(y)+3.2
+        # 1-D interpolation
+        xp = @inferred interpolate(x, i)
+        @test xp ≈ first(xp):step(x)*step(i):last(xp)
+
+        # 2-D interpolation
+        f(x::Real, y::Real) = sin(x*x + y)
+        z = f.(x, y')
+        @test z === @inferred interpolate(z, :, :)
+        @test z === @inferred interpolate(z, :)
+        @test z === @inferred interpolate(z)
+        r1 = @inferred interpolate(z, i, :)
+        @test r1 == @inferred interpolate(z, i)
+        r2 = @inferred interpolate(r1, :, j)
+        r3 = @inferred interpolate(z, i, j)
+        @test r2 == r3
+    end
 end
