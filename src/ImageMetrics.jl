47dc7fe819eb8f5d02e45544646edca7319d660c
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2024-06-14 10:49:35 +0200

    Add `map_with_offsets` method

--- src/ImageMetrics.jl
+++ src/ImageMetrics.jl
@@ -5,10 +5,11 @@ export
     bounding_box,
     crop, crop!,
     interpolate, interpolate!,
+    map_with_offsets, map_with_offsets!,
     resample,
     zerofill!
 
-using TypeUtils, ArrayTools, EasyRanges
+using TypeUtils, ArrayTools, EasyRanges, OffsetArrays
 
 include("utils.jl")
 include("metrics.jl")
--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,95 @@
 """
+    map_with_offsets(f, A, B, inds...; A_pad=zero(eltype(A)), B_pad=zero(eltype(B))) -> C
+
+yields an array `C` with axes `inds...` such that:
+
+    C[k] =   sum_{j ∈ (I + k) ∩ J} f(A[j-k], B[j])
+           + sum_{i ∈ I \\ (I ∩ (J - k))} f(A[i], B_pad)
+           + sum_{j ∈ J \\ ((I + k) ∩ J)} f(A_pad, B[j])
+
+where `I`, `J`, and `K` are the respective Cartesian indices of arrays `A`, `B`, and `C`
+while `A_pad` and `B_pad` are the padding values assumed for `A[i]` and `B[j]` for
+out-of-bound indices `i` and/or `j`.
+
+The function `f` must be such that `iszero(f(A_pad, B_pad))` holds.
+
+"""
+function map_with_offsets(f,
+                          A::AbstractArray{<:Any,N},
+                          B::AbstractArray{<:Any,N},
+                          inds::AbstractUnitRange{<:Integer}...; kwds...) where {N}
+    return map_with_offsets(f, A, B, inds; kwds...)
+end
+
+function map_with_offsets(f,
+                          A::AbstractArray{<:Any,N},
+                          B::AbstractArray{<:Any,N},
+                          inds::NTuple{N,AbstractUnitRange{<:Integer}}; kwds...) where {N}
+    return map_with_offsets(f, A, B, map(to_axis, inds); kwds...)
+end
+
+function map_with_offsets(f,
+                          A::AbstractArray{<:Any,N},
+                          B::AbstractArray{<:Any,N},
+                          inds::NTuple{N,AbstractUnitRange{Int}}; kwds...) where {N}
+    T = float(promote_type(eltype(A), eltype(B)))
+    C = OffsetArray(Array{T,N}(undef, map(length, inds)), map(first, inds))
+    return map_with_offsets!(C, f, A, B; kwds...)
+end
+
+"""
+    map_with_offsets!(C, f, A, B; A_pad=zero(eltype(A)), B_pad=zero(eltype(B))) -> C
+
+overwrites array `C` with:
+
+    C[k] =   sum_{j ∈ (I + k) ∩ J} f(A[j-k], B[j])
+           + sum_{i ∈ I \\ (I ∩ (J - k))} f(A[i], B_pad)
+           + sum_{j ∈ J \\ ((I + k) ∩ J)} f(A_pad, B[j])
+
+where `I`, `J`, and `K` are the respective Cartesian indices of arrays `A`, `B`, and `C`
+while `A_pad` and `B_pad` are the padding values assumed for `A[i]` and `B[j]` for
+out-of-bound indices `i` and/or `j`.
+
+The function `f` must be such that `iszero(f(A_pad, B_pad))` holds.
+
+"""
+function map_with_offsets!(C::AbstractArray{<:Any,N},
+                           f,
+                           A::AbstractArray{<:Any,N},
+                           B::AbstractArray{<:Any,N};
+                           A_pad = zero(eltype(A)),
+                           B_pad = zero(eltype(B))) where {N}
+    A_pad = as(eltype(A), A_pad)
+    B_pad = as(eltype(B), B_pad)
+    f_zero = f(A_pad, B_pad)
+    iszero(f_zero) || throw(AssertionError(
+        "`iszero(f(A_pad,B_pad))` does not hold with `A_pad = $(A_pad)` and `B_pad = $(B_pad)`"))
+    I = bounding_box(x -> x != A_pad, A)
+    J = bounding_box(x -> x != B_pad, B)
+    K = CartesianIndices(C)
+    A_out = f_zero
+    @inbounds for i in I
+        A_out += f(A[i], B_pad)
+    end
+    B_out = f_zero
+    @inbounds for j in J
+        B_out += f(A_pad, B[j])
+    end
+    f_out = A_out + B_out
+    @inbounds for k in K
+        f_in = f_zero
+        for j in @range (I + k) ∩ J
+            i = j - k
+            A_i = A[i]
+            B_j = B[j]
+            f_in += f(A_i, B_j) - (f(A_i, B_pad) + f(A_pad, B_j))
+        end
+        C[k] = f_in + f_out
+    end
+    return C
+end
+
+"""
     zerofill!(A) -> A
 
 fills array `A` with zeros and returns `A`.
@@ -164,3 +255,27 @@ following the same conventions as `fftshift`.
 central_axis_index(dim::Integer) = div(as(Int, dim), 2) + 1
 central_axis_index(rng::AbstractUnitRange{<:Integer}) =
     div(as(Int, first(rng)) + as(Int, last(rng)) + 1, 2)
+
+"""
+    ImageMetrics.to_axis(dim::Integer)
+    ImageMetrics.to_axis(rng::AbstractUnitRange{<:Integer})
+
+yield array axis (as an instance of `AbstractUnitRange{Int}`) corresponding to array
+dimension `dim` or to array index range `rng`. In the former case, 1-based indices are
+assumed.
+
+"""
+to_axis(dim::Integer) = Base.OneTo{Int}(dim)
+to_axis(rng::AbstractUnitRange{Int}) = rng
+to_axis(rng::AbstractUnitRange{<:Integer}) = convert_eltype(Int, rng)
+
+"""
+    ImageMetrics.to_dim(dim::Integer)
+    ImageMetrics.to_dim(rng::AbstractUnitRange{<:Integer})
+
+yield array dimension length (an `Int`) corresponding to array dimension `dim` or to array
+index range `rng`.
+
+"""
+to_dim(dim::Integer) = as(Int, dim)
+to_dim(rng::AbstractUnitRange{Int}) = as(Int, length(rng))
